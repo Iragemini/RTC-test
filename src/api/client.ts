9cc893b3a0ea0f108e01bf835690e3838ed3c726
Irina Karavaichyk <irinakaravaichyk@mac-1.home>
2025-03-24 20:26:18 +0100

    docs: add method descriptions

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,15 +4,24 @@ import { IMappings, ISimulationAPIClient, IState } from './types';
 export const GET_STATE_POSTFIX = 'api/state';
 export const GET_MAPPINGS_POSTFIX = 'api/mappings';
 
+/**
+ * Create a new instance of SimulationAPIClient.
+ */
 export default class SimulationAPIClient implements ISimulationAPIClient {
   constructor(private baseUrl: string) {}
 
+  /**
+   * Fetch state from API
+   */
   async getState(): Promise<IState> {
     const { data } = await axios.get(`${this.baseUrl}/${GET_STATE_POSTFIX}`);
 
     return data;
   }
 
+  /**
+   * Fetch mappings from API
+   */
   async getMappings(): Promise<IMappings> {
     const { data } = await axios.get(`${this.baseUrl}/${GET_MAPPINGS_POSTFIX}`);
 
