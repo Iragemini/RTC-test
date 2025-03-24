3ed0b059b50bf131f192625f88a3d6f6db34e34f
Irina Karavaichyk <irinakaravaichyk@mac-1.home>
2025-03-24 15:55:17 +0100

    test: add test on API errors

--- test/api/client.test.ts
+++ test/api/client.test.ts
@@ -5,21 +5,29 @@ import mockState from './__mocks__/data/state';
 
 vi.mock('axios');
 
-const baseUrl = 'http//localhost:3000';
+const BASE_URL = 'http//localhost:3000';
+const API_ERROR = 'An error occurred while calling the simulation API.';
 
 describe('Simulated API Client', () => {
   let client: SimulationAPIClient;
 
   beforeEach(() => {
     vi.clearAllMocks();
-    client = new SimulationAPIClient(baseUrl);
+    client = new SimulationAPIClient(BASE_URL);
   });
   test('Should fetch state from API', async () => {
     vi.mocked(axios.get).mockResolvedValueOnce({ data: mockState });
 
     const result = await client.getState();
 
-    expect(axios.get).toHaveBeenCalledWith(`${baseUrl}/${GET_STATE_POSTFIX}`);
+    expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/${GET_STATE_POSTFIX}`);
     expect(result).toEqual(mockState);
+  });
+
+  test('should handle API errors', async () => {
+    vi.mocked(axios.get).mockRejectedValueOnce(new Error(API_ERROR));
+
+    await expect(client.getState()).rejects.toThrow(API_ERROR);
+    expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/${GET_STATE_POSTFIX}`);
   });
 });
